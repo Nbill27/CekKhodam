41cbb774489f064538371f3be5152ab991d66dd7
Elbill <108813177+Nbill27@users.noreply.github.com>
2024-06-22 21:15:45 +0800

    Update ini.js

--- ini.js
+++ ini.js
@@ -1,26 +1,26 @@
 const khodams = [
-    "Air Cucian Beras", "Api neraka", "Ampas kopling", "Batu Karang", "Cahaya Senter", 
-    "Daun Rimbun", "mimi peri", "Kembang Pasir", "Hujan Deras", "Kera Sakti", 
-    "Pak Vincent", "Tanah Kubur", "Ayam Sakit", "Titid Terbang", "Layangan Putus", 
-    "penunggu taman lawang", "Buaya Putih", "Kuntilanak Merah", "Pocong gila", "Tuyul Emas"
+  "Air Cucian Beras", "Api neraka", "Ampas kopling", "Batu Karang", "Cahaya Senter", 
+  "Daun Rimbun", "mimi peri", "Kembang Pasir", "Hujan Deras", "Kera Sakti", 
+  "Pak Vincent", "Tanah Kubur", "Ayam Sakit", "Titid Terbang", "Layangan Putus", 
+  "penunggu taman lawang", "Buaya Putih", "Kuntilanak Merah", "Pocong gila", "Tuyul Emas"
 ];
 
 function checkKhodam() {
-    const nameInput = document.getElementById('nameInput').value;
-    if (nameInput.trim() === "") {
-        alert("Tulis nama kamu di sini!");
-        return;
-    }
-    const randomIndex = Math.floor(Math.random() * khodams.length);
-    const khodam = khodams[randomIndex];
-    document.getElementById('resultText').innerText = `Khodam yang ada di dalam diri kamu: ${khodam}`;
-    document.getElementById('shareText').style.display = 'block';
-    document.querySelector('.button-container').style.display = 'flex';
+  const nameInput = document.getElementById('nameInput').value;
+  if (nameInput.trim() === "") {
+    alert("Tulis nama kamu di sini!");
+    return;
+  }
+  const randomIndex = Math.floor(Math.random() * khodams.length);
+  const khodam = khodams[randomIndex];
+  document.getElementById('resultText').innerText = `Khodam yang ada di dalam diri kamu: ${khodam}`;
+  document.getElementById('shareText').style.display = 'block';
+  document.getElementById('retryButton').style.display = 'block'; // Menampilkan tombol Ulangi
 }
 
 function retry() {
-    document.getElementById('nameInput').value = '';
-    document.getElementById('resultText').innerText = '';
-    document.getElementById('shareText').style.display = 'none';
-    document.querySelector('.button-container').style.display = 'none';
+  document.getElementById('nameInput').value = '';
+  document.getElementById('resultText').innerText = '';
+  document.getElementById('shareText').style.display = 'none';
+  document.getElementById('retryButton').style.display = 'none'; // Menyembunyikan tombol Ulangi
 }
