aff8ac71ae61f7a5a0c67e9b390d8a5ab247a301
Elbill <108813177+Nbill27@users.noreply.github.com>
2024-06-22 20:57:07 +0800

    Update ini.js

--- ini.js
+++ ini.js
@@ -1,8 +1,8 @@
 const khodams = [
-    "Air Cucian Beras", "Api neraka", "Ampas kopling", "Batu Karang", "Cahaya Bulan", 
+    "Air Cucian Beras", "Api neraka", "Ampas kopling", "Batu Karang", "Cahaya Senter", 
     "Daun Rimbun", "mimi peri", "Kembang Pasir", "Hujan Deras", "Kera Sakti", 
-    "Pak Vincent", "Tanah Kubur", "Sepeda Ontel", "Titid Terbang", "Layangan Putus", 
-    "Baju Robek", "Buaya Putih", "Kuntilanak Merah", "Pocong gila", "Tuyul Emas"
+    "Pak Vincent", "Tanah Kubur", "Ayam Sakit", "Titid Terbang", "Layangan Putus", 
+    "penunggu taman lawang", "Buaya Putih", "Kuntilanak Merah", "Pocong gila", "Tuyul Emas"
 ];
 
 function checkKhodam() {
@@ -15,12 +15,12 @@ function checkKhodam() {
     const khodam = khodams[randomIndex];
     document.getElementById('resultText').innerText = `Khodam yang ada di dalam diri kamu: ${khodam}`;
     document.getElementById('shareText').style.display = 'block';
-    document.getElementById('retryButton').style.display = 'block';
+    document.querySelector('.button-container').style.display = 'flex';
 }
 
 function retry() {
     document.getElementById('nameInput').value = '';
     document.getElementById('resultText').innerText = '';
     document.getElementById('shareText').style.display = 'none';
-    document.getElementById('retryButton').style.display = 'none';
+    document.querySelector('.button-container').style.display = 'none';
 }
